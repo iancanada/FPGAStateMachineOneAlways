906155a2b0a6460408fcbdc619e2a6492177851b
Ian <iancanada.mail@gmail.com>
2015-09-07 15:07:27 -0400

    Update StateMachineExample.v

--- StateMachineExample.v
+++ StateMachineExample.v
@@ -11,13 +11,11 @@
   */
 
 module SourceSelectionControl(input reset,                //system reset
-										input clk,                  //system clock
-										input select,               //selection jumper, high as default duo to internal weak pull-up
-										output reg selectionresult, //slection result after removing vibration
+		                      input clk,                  //system clock
+			                  input select,               //selection jumper, high as default duo to internal weak pull-up
+			                  output reg selectionresult, //slection result after removing vibration
                               output reg selectionchanged //changed flag, 65536 clock signal width to indicate selection has just been changed
-									  );
-
-
+			                  );
 reg[15:0] counter;           //count the mclk to run state machin at very low speed
 
 //run 16bit counter,the max time for for 49.1520 clk is 65535/49.1520M=1.33ms
@@ -33,16 +31,16 @@ end
 reg[4:0] state;
 parameter[4:0] BEGIN   =0,
                SELECT1 =1,
-					CHANGE1 =2,
-					SELECT0 =3,
-					CHANGE0 =4;
+	           CHANGE1 =2,
+	           SELECT0 =3,
+	           CHANGE0 =4;
 
 //FSM in one always style with indexed code
 always@(posedge clk or negedge reset)
 begin
   if(!reset) begin
     selectionresult<=0;                      //power up default is 0
-	 selectionchanged<=0;                     //power up default is 0
+    selectionchanged<=0;                     //power up default is 0
     state<=BEGIN;
   end
   else begin
@@ -50,7 +48,7 @@ begin
 	   case(state)
 		  BEGIN: begin
 		    selectionresult<=selectionresult;  //keep selection result no change 
-			 selectionchanged<=1;               //indicate slection changed to perform frontend reset, 1 only in here, in all other states is 0
+			selectionchanged<=1;               //indicate slection changed to perform frontend reset, 1 only in here, in all other states is 0
 		    state<=select? SELECT1:SELECT0;    //next state decided by select input
 		    end
         SELECT1: begin
